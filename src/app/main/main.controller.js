e2925ff4c76fd691c9fcf5c498a035cdeb482259
PavloM7896 <pasha80676222267@gmail.com>
2017-02-16 22:33:16 +0200

    new version

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,40 +6,15 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr,$http,$scope) {
+  function MainController($http,$scope) {
     var vm = this;
     function getTodos(){
       return $http.get('https://test-node-js-ua.herokuapp.com/todos')
     }
     getTodos().then(function (res) {
-      $scope.todos = res.data.todos;
-      
+      $scope.todos = res.data;
+      console.log($scope.todos)
     })
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1486933478140;
-    vm.showToastr = showToastr;
 
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
